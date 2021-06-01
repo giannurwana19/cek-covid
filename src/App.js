85623856649e5e840bcb22c6c359988dcec1a13a
giannurwana19 <giannurwana19@gmail.com>
2021-06-01 13:33:34 +0700

    make component cards

--- src/App.js
+++ src/App.js
@@ -1,16 +1,33 @@
+import React, { Component } from 'react';
 import Cards from './components/Cards';
 import Chart from './components/Chart';
 import CountryPicker from './components/CountryPicker';
 import styles from './App.module.css';
+import { fetchData } from './api';
 
-function App() {
-  return (
-    <div className={styles.container}>
-      <Cards />
-      <Chart />
-      <CountryPicker />
-    </div>
-  );
+class App extends Component {
+  state = {
+    data: {},
+  };
+
+  async componentDidMount() {
+    const data = await fetchData();
+
+    this.setState({ data });
+    console.log(data);
+  }
+
+  render() {
+    const { data } = this.state;
+
+    return (
+      <div className={styles.container}>
+        <Cards data={data} />
+        <Chart />
+        <CountryPicker />
+      </div>
+    );
+  }
 }
 
 export default App;
