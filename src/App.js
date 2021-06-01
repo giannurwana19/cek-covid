7e0fdc3f43af11e149adb3c51f4cf8a008c7f500
giannurwana19 <giannurwana19@gmail.com>
2021-06-01 11:28:42 +0700

    install package & make basic component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import Cards from './components/Cards';
+import Chart from './components/Chart';
+import CountryPicker from './components/CountryPicker';
+import styles from './App.module.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={styles.container}>
+      <Cards />
+      <Chart />
+      <CountryPicker />
     </div>
   );
 }
---
+++ src/components/index.js
@@ -0,0 +1,6 @@
+export { default as Cards } from './Cards';
+export { default as CountryPicker } from './CountryPicker';
+export { default as Chart } from './Chart';
+
+// saat export nya kita ke App.js seperti ini:
+// import { Cards, Chart, CountryPicker } from './components';
